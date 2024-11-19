90406f54da7f8261fc0c4fbc75499eee5298e610
MunawarAziz <aziz09126789@gmail.com>
2024-11-19 13:21:34 +0500

    complet project

--- 30DaysOfProjects/Auto Text Animation/script.js
+++ 30DaysOfProjects/Auto Text Animation/script.js
@@ -1,1 +1,24 @@
 const container = document.querySelector(".container")
+
+const careers = ['instructor', 'tiktoker', 'web developer', 'free lancer']
+
+let careerindex = 0;
+
+let charactorindex = 0;
+
+updateText()
+
+function updateText(){
+    charactorindex++
+    container.innerHTML = `<h1>I am  ${careers[careerindex].slice(0,charactorindex)}</h1>`;
+    
+    if(charactorindex === careers[careerindex].length){
+        careerindex++
+        charactorindex = 0
+    }
+    if(careerindex === careers.length){
+        careerindex = 0;
+    }
+    setTimeout(updateText ,400 );
+}
+
