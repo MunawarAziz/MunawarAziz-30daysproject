fdc613447d14d31fbe5cd411bf245057e03c23bc
MunawarAziz <aziz09126789@gmail.com>
2024-11-28 10:57:05 +0500

    changes in script file

--- 30DaysOfProjects/Random Photos/script.js
+++ 30DaysOfProjects/Random Photos/script.js
@@ -1,33 +1,18 @@
-// const images = document.querySelector(".image-container")
+const imageConatiner = document.querySelector(".images-container")
 
-// const btn = document.querySelector(".btn")
+const btn = document.querySelector(".btn")
 
-// btn.addEventListener("click",()=>{
-//     console.log("click")
-//     addNewPic()
-// });
-
-// function addNewPic(){
-//     const newimg = document.createElement("img");
-//     newimg.src =`https://picsum.photos/300?random=${Math.floor(Math.random()*2000)}`
-//     images.appendChild(newimg);
-// };
-const images = document.querySelector(".image-container");
-const btn = document.querySelector(".btn");
-
-btn.addEventListener("click", () => {
-    console.log("click");
-    addNewPic();
+btn.addEventListener("click",()=>{
+    imgnum = 5;
+    addNewPic()
 });
 
-function addNewPic() {
-    const newimg = document.createElement("img");
-    newimg.src = `https://picsum.photos/300?random=${Math.floor(Math.random() * 2000)}`;
-    
-    // Handle error if the image fails to load
-    newimg.onerror = () => {
-        newimg.src = "path/to/placeholder/image.jpg"; // Provide a fallback image
-    };
-    
-    images.appendChild(newimg);
-}
+function addNewPic(){
+    for (let index = 0; index < imgnum; index++) {
+        const newimg = document.createElement("img");
+    newimg.src =`https://picsum.photos/300?random=${Math.floor(Math.random()*2000)}`
+    imageConatiner.appendChild(newimg);
+        
+    }
+   
+};
