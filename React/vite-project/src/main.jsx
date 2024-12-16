686f8cb474c79f1662c58000abf02403983bbb79
MunawarAziz <aziz09126789@gmail.com>
2024-12-16 15:03:43 +0500

    React

---
+++ React/vite-project/src/main.jsx
@@ -0,0 +1,7 @@
+
+import ReactDOM from 'react-dom/client';  // Ensure you're importing createRoot from the right place
+import App from './App.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <App />
+);
