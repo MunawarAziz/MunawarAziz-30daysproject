697bd4258032f2949ea28f1e6a01cc991dc8bcac
MunawarAziz <149875349+MunawarAziz@users.noreply.github.com>
2024-12-16 15:24:23 +0500

    Delete React01-main directory

--- React01-main/01basic/src/Test.js
+++
@@ -1,8 +0,0 @@
-function Test() {
-
-    return(
-        <h1>Testing In React</h1>
-    )
-}
-
-export default Test
--- React01-main/vite-project/src/Test.jsx
+++
@@ -1,9 +0,0 @@
-function Test(){
-
-    return(
-        <h3>Just Testing In Vite React</h3>
-    )
-};
-
-export default Test
-
